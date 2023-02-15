eb76ce8c2aa1b607e065b6f3ce5c0c74aa210fd0
GreXLin85 <grexlin85@gmail.com>
2023-02-15 22:19:38 +0200

    some testing...

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,27 @@
+import bodyParser from 'body-parser';
 import express from 'express';
+import { TypedRequestBody } from './types/TypedRequestBody';
 
 const app = express();
 
-app.post('/shorturl', (req, res) => {
-    const = req.body.url;
-    
+app.use(bodyParser.json())
+
+app.post('/shorturl', (req: TypedRequestBody<{
+    url: string;
+}>, res) => {
+    const url = req.body.url
+
+    // check if url valid with this regex https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)
+
+    if(!new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g).test(url)){
+        res.status(400).send('Invalid url')
+    }
+        
+
+    res.send(url);
+});
+
+const PORT = process.env.SERVER_PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
