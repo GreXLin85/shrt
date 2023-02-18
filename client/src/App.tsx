689d0c2798a40c44f8867db45e831eb0b41ed8de
GreXLin85 <grexlin85@gmail.com>
2023-02-18 15:34:33 +0200

    homepage completed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Flex, Paper, Title, TextInput, Button } from '@mantine/core';
+import { useClipboard } from '@mantine/hooks';
+import { showNotification } from '@mantine/notifications';
+import { useRef, useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState<string>('')
+  const inputReference = useRef(null);
+  const clipboard = useClipboard({ timeout: 500 });
+
+  const handleShortIt = () => {
+    if (value.length > 2048) {
+      return showNotification({
+        message: 'URL is too long',
+      });
+    }
+
+    if (!new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)/g).test(value)) {
+      return showNotification({
+        message: 'It is not a valid URL',
+      });
+    }
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({
+      "url": value
+    });
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+    };
+
+    fetch("http://localhost:3000/createLink", requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        showNotification({
+          message: "Your URL Shortened and Copied!",
+        });
+        setValue(`http://localhost:3000/${result.message.accessToken}`);
+        clipboard.copy(value);
+        (inputReference?.current as any).select();
+
+      })
+      .catch(error => console.log('error', error));
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Flex
+      mih={50}
+      gap="lg"
+      justify="flex-start"
+      align="center"
+      direction="column"
+    >
+      <Title>SHRT.IO</Title>
+
+      <Paper shadow="xs" radius="lg" p="md" withBorder>
+        <Flex
+          mih={50}
+          gap="lg"
+          justify="flex-start"
+          align="center"
+          direction="column"
+        >
+          <TextInput label="Your URL" value={value} ref={inputReference} onClick={() => {
+            (inputReference?.current as any).select();
+            clipboard.copy(value);
+
+          }} onChange={(e) => {
+            setValue(e.currentTarget.value)
+          }} placeholder="https://example.com" size="md" />
+          <Button variant="gradient" gradient={{ from: 'teal', to: 'blue', deg: 60 }} onClick={() => handleShortIt()}>Short It!</Button>
+        </Flex>
+
+      </Paper>
+    </Flex>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MantineProvider withGlobalStyles withNormalizeCSS>
+    <NotificationsProvider>
+      <App />
+    </NotificationsProvider>
+  </MantineProvider>,
 )
